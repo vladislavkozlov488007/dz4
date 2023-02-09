62728b221cf6e657a3cb06e7384d5317ac4d4d5a
vladislavkozlov488007 <117895583+vladislavkozlov488007@users.noreply.github.com>
2023-02-09 17:40:47 +0300

    z29

--- Program.cs
+++ Program.cs
@@ -1,20 +1,15 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
-System.Console.WriteLine("Введите целое число");
-int n = Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"Сумма цифр в веденном числе равна: {SumDigits(n)}");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+System.Console.WriteLine("Введите 8 чисел с клавиатуры");
+int[] array = new int[8];
+for (int i=0; i<8; i++)
+array[i] = Convert.ToInt32(Console.ReadLine());
+PrintArray(array);
 
-int SumDigits(int n)
-    {
-        int sum = 0;
-        int result = 0;
-        for (int i = 1; n > 0; i++) 
-        {
-            result = n%10;
-            sum = sum + result;
-            n/=10;
-        }
-        return sum;
-    }
+void PrintArray(int[] a)
+{
+    for (int i=0; i<a.Length-1; i++)
+    System.Console.Write($"{a[i]}, ");
+    System.Console.WriteLine($"{a[^1]}");
+}
